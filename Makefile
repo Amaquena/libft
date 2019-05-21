d0c6f1a0ebbdbe16c78fcf4cad0d813d9e7b8566
Alvin MAQUENA <amaquena@c3r8s3.wethinkcode.co.za>
2019-05-21 13:46:23 +0200

    update, bug and norm fixes

--- Makefile
+++ Makefile
@@ -6,26 +6,27 @@
 #    By: event <marvin@42.fr>                       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/05/20 10:32:53 by event             #+#    #+#              #
-#    Updated: 2019/05/20 13:40:17 by event            ###   ########.fr        #
+#    Updated: 2019/05/21 13:42:52 by amaquena         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME = libft.a
-CFILES = *.c
-OFILES = *.o
+CFILES = ft_strlen.c
+OFILES = ft_strlen.o
 
 $(NAME):
 	gcc -c -Wall -Wextra -Werror $(CFILES)
-	ar rc $(NAME) $(CFILES)
+	ar rcs $(NAME) $(OFILES)
 	ranlib $(NAME)
 
 all: $(NAME)
 clean:
-	rm -r *o
+	rm -r $(OFILES)
 fclean: clean
 	rm -r *.a
+	rm -f a.out
 re: all fclean
 
-*clean remove unneccessary files
-*re make then clean to remove un files rm a.out & .o
-*remove exe file & all o files feclean make & fclean
+#clean remove unneccessary files
+#re make then clean to remove un files rm a.out & .o
+#remove exe file & all o files feclean make & fclean
--- libft.h
+++ libft.h
@@ -1,8 +1,19 @@
-#ifndef _LIBFT_H_
-# define _LIBFT_H_
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amaquena <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/21 12:24:43 by amaquena          #+#    #+#             */
+/*   Updated: 2019/05/21 12:26:35 by amaquena         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include <string.h>
-#include <unistd.h>
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <string.h>
+# include <unistd.h>
 
 //memset;
 //bzero;
@@ -11,7 +22,7 @@
 //memmove;
 //memchr;
 //memcmp;
-size_t	ft_strlen (const char *s);
+size_t	ft_strlen(const char *s);
 //strdup;
 //strcpy;
 //strncpy;
