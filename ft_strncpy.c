eacd61df95e28f7bfa2c8636990f9044f6423d4e
Alvin MAQUENA <amaquena@c3r8s3.wethinkcode.co.za>
2019-05-22 12:35:03 +0200

    ft_strncpy.c update, if statment removed

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: amaquena <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 16:33:54 by amaquena          #+#    #+#             */
-/*   Updated: 2019/05/22 09:49:25 by amaquena         ###   ########.fr       */
+/*   Updated: 2019/05/22 12:34:09 by amaquena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ char	*ft_strncpy(char *dst, const char *src, size_t len)
 {
 	size_t i;
 
-	if (dst == NULL || src == NULL)
-		return (NULL);
 	i = 0;
 	while ((i < len) && (src[i] != '\0'))
 	{
