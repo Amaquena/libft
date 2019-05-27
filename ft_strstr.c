2c623632d4812e08c4749750fef4ad7c1858ad1a
Alvin MAQUENA <amaquena@c3r8s3.wethinkcode.co.za>
2019-05-27 13:47:22 +0200

    ft_strstr.c created

---
+++ ft_strstr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amaquena <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/27 11:49:12 by amaquena          #+#    #+#             */
+/*   Updated: 2019/05/27 13:46:55 by amaquena         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strstr(const char *haystack, const char *needle)
+{
+	int i;
+	int j;
+
+	if (needle == NULL)
+		return ((char *)needle);
+	i = 0;
+	j = 0;
+	while (haystack[i] != '\0')
+	{
+		while (needle[j] == haystack[i + j])
+		{
+			if (needle[j + 1] == '\0')
+				return ((char*)haystack + i);
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
+}
