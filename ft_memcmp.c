38662a85c24cd0d56f6250e35973e9195d6af0e7
Alvin MAQUENA <amaquena@c3r8s3.wethinkcode.co.za>
2019-05-29 12:34:11 +0200

    ft_memcmp.c created

---
+++ ft_memcmp.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amaquena <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/29 11:48:17 by amaquena          #+#    #+#             */
+/*   Updated: 2019/05/29 12:32:55 by amaquena         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	while ((*(unsigned char *)s1 && *(unsigned char *)s2) && (*(unsigned char *)s1 == *(unsigned char *)s2) && n)
+	{
+		s1++;
+		s2++;
+		n--;
+	}
+	if (n == 0)
+		return (0);
+	else
+		return (*(unsigned char *)s1 - *(unsigned char *)s2);
+	return (0);
+}
