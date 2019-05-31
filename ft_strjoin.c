4854e6ce820fa79f06c32685f50efb45840a4ce1
Alvin MAQUENA <amaquena@c3r8s3.wethinkcode.co.za>
2019-05-31 13:39:03 +0200

    ft_strjion.c

---
+++ ft_strjoin.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amaquena <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/31 13:11:42 by amaquena          #+#    #+#             */
+/*   Updated: 2019/05/31 13:38:35 by amaquena         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char *str;
+
+	if ((!s1 || !s2) || !(str = ft_strnew(ft_strlen(s1) + ft_strlen(s2))))
+			return (NULL);
+	str = strcpy(str, s1);
+	if (!(str = ft_strcat((char *)str, (char *)s2)))
+		return (NULL);
+	else
+		return (str);
+}
