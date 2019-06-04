3729f7d4d871cc1c77fc1510dd490009f2d69a2d
Alvin MAQUENA <amaquena@c3r6s3.wethinkcode.co.za>
2019-06-04 14:54:53 +0200

    ft_strchr.c fixed

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: amaquena <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 11:31:07 by amaquena          #+#    #+#             */
-/*   Updated: 2019/05/24 15:27:45 by amaquena         ###   ########.fr       */
+/*   Updated: 2019/06/04 14:53:54 by amaquena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 char	*ft_strchr(const char *s, int c)
 {
 	unsigned char chr;
+	int i;
 
-	chr = c;
-	while (s)
-	{
-		if (*s == chr)
-			return ((char*)s);
-		s++;
-	}
+	i = 0;
+	chr = (unsigned char)c;
+	while (s[i] && s[i] != c)
+		i++;
+	if (s[i] == chr)
+		return ((char *)s + i);
 	return (NULL);
 }
