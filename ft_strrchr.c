2d8f082d8018a192ebcfa15f1c272df7fdd82977
Alvin MAQUENA <amaquena@c3r6s3.wethinkcode.co.za>
2019-06-04 15:12:32 +0200

    ft_strrchr.c fixes

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: amaquena <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 15:34:21 by amaquena          #+#    #+#             */
-/*   Updated: 2019/05/27 10:57:10 by amaquena         ###   ########.fr       */
+/*   Updated: 2019/06/04 15:11:49 by amaquena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,14 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	size_t i;
+	int end;
 	unsigned char chr;
 
-	i = ft_strlen(s);
-	s += i;
-	chr = c;
-	while (i > 0)
-	{
-		if (*s == chr)
-			return ((char*)s);
-		s--;
-		i--;
-	}
+	chr = (unsigned char)c;
+	end = ft_strlen(s);
+	while (s[end] != c && end > 0)
+		end--;
+	if (s[end] == c)
+		return ((char *)s + end);
 	return (NULL);
 }
