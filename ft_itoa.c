98ad6f5c0ff62f1ac70aed071a13316b5b1b26df
Alvin MAQUENA <amaquena@c3r6s3.wethinkcode.co.za>
2019-06-07 17:31:32 +0200

    ft_itoa.c fixed

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: amaquena <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 16:18:16 by amaquena          #+#    #+#             */
-/*   Updated: 2019/06/06 17:30:41 by amaquena         ###   ########.fr       */
+/*   Updated: 2019/06/07 16:08:00 by amaquena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,34 @@
 
 char	*ft_itoa(int n)
 {
+	char *num;
+	int i;
+	int numlen;
 
+	if (n == INT_MIN)
+		return (ft_strdup("-2147483648"));
+
+	numlen = ft_numlen(n);
+	if (!(num = ft_strnew(numlen)))
+		return (NULL);
+
+	i = 0;
+	if (n < 0)
+	{
+		num[i++] = '-';
+		n *= -1;
+	}
+	num[numlen--] = '\0';
+	if (n > 0)
+	{
+		while (numlen > 0)
+		{
+			num[numlen] = (n % 10) + '0';
+			n /= 10;
+			numlen--;
+		}
+	}
+	if (num[numlen] != '-')
+		num[numlen] = (n % 10) + '0';
+	return (num);
 }
