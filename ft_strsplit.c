8ba32d6e7edbb4e8b39f9ebe5595623dffbdf2b5
Alvin MAQUENA <amaquena@c3r6s3.wethinkcode.co.za>
2019-06-10 14:38:06 +0200

    created

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,27 +6,102 @@
 /*   By: amaquena <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 16:11:53 by amaquena          #+#    #+#             */
-/*   Updated: 2019/06/07 17:24:12 by amaquena         ###   ########.fr       */
+/*   Updated: 2019/06/10 14:34:24 by amaquena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	**ft_strsplit(char const *s, char c)
+static void	ft_freetab(char **tab, int pos)
 {
-	char *str;
+	int i;
 
+	i = -1;
+	while (++i < pos)
+		free(tab[i]);
+}
 
-	while (s)
+static int		ft_wordcount(char const *str, char c)
+{
+	int i;
+	int count;
+
+	i = 0;
+	count = 0;
+	while (str[i] != '\0')
 	{
-		i = 0;
-		while (s[i] != c)
+		while (str[i] == c && str[i] != '\0')
 			i++;
-		str = ft_str(s, i);
+		if (str[i] != c && str[i] != '\0')
+			count++;
+		while (str[i] != c && str[i] != '\0')
+			i++;
 	}
+	return (count);
 }
 
-char * ft_str(char const *str, int i)
+static int	ft_wordlen(char const *str, char c, int wdpos)
 {
-	
+	int count;
+
+	count = 0;
+	while (str[wdpos] != c && str[wdpos] != '\0')
+	{
+		count++;
+		wdpos++;
+	}
+	return (count);
 }
+
+char		**ft_strsplit(char const *s, char c)
+{
+    int i;
+    int j;
+    int k;
+    int wlen;
+    int wcount;
+    char **str;
+    
+    wcount = ft_wordcount(s,c);
+    if ( !c || !s || !(str = (char **)malloc(sizeof(char *) * wcount)))
+        return (NULL);
+    i = 0;
+    j = 0;
+    while (s[i] != '\0')
+    {
+        while (s[i] == c)
+            i++;
+        wlen = ft_wordlen(s, c, i);
+        while (j < wcount && s[i] != c)
+        {
+            if (!c || !s || !(str[j] = (char *)malloc(sizeof(char) * wlen + 1)))
+			{
+				ft_freetab(str, j);
+				free(str);
+                return (NULL);
+			}
+            k = 0;
+            while (wlen > 0)
+            {
+                str[j][k] = s[i];
+                k++;
+                i++;
+                wlen--;
+            }
+            str[j][k] = '\0';
+            j++;
+        }
+    }
+    return (str);
+}
+/*
+int main (void)
+{
+	wc = 0;
+	int	wl = 0;
+	char *a = "*Hello**fellow**student*";
+
+	wc = ft_wordcount(a,'l');
+	wl = ft_wordlen(a, '*', 16);
+	printf("%d", wl);
+}*/
